008ecf5419f1f99722f3261f8fe5a9d6ab351826
RinatSa <sadykov.r@seznam.cz>
2023-06-07 19:44:09 +0200

    Update: add state and clickOnItem function, add another two rows for starships and planets

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,19 @@ import Header from "./components/header.jsx";
 import RandomPlanet from "./components/random-planet.jsx";
 import List from "./components/list.jsx";
 import Info from "./components/info.jsx";
+import ApiService from "./apiService.js";
 
 class App extends Component {
+
+    apiService = new ApiService()
+
+    state = {
+        chosenId: 1
+    }
+
+    clickOnItem = (id) => {
+        this.setState({chosenId: id})
+    }
 
     render() {
         return (
@@ -13,8 +24,16 @@ class App extends Component {
                 <Header/>
                 <RandomPlanet/>
                 <div className="row justify-content-between">
-                    <List/>
-                    <Info/>
+                    <List clickOnItem={this.clickOnItem} getData={this.apiService.getAllPersons}/>
+                    <Info id={this.state.chosenId}/>
+                </div>
+                <div className="row justify-content-between">
+                    <List clickOnItem={this.clickOnItem} getData={this.apiService.getAllStarships}/>
+                    <Info id={this.state.chosenId}/>
+                </div>
+                <div className="row justify-content-between">
+                    <List clickOnItem={this.clickOnItem} getData={this.apiService.getAllPlanets}/>
+                    <Info id={this.state.chosenId}/>
                 </div>
             </div>
         )
