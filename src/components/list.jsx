46466f5116284ec83e828448e730695315d3aa85
RinatSa <sadykov.r@seznam.cz>
2023-06-07 18:43:35 +0200

    Add person list

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,10 +1,34 @@
 import {Component} from 'react';
+import ApiService from "../apiService.js";
 
 class List extends Component {
+
+    apiService = new ApiService()
+
+    state = {
+        list: []
+    }
+
+    componentDidMount() {
+        this.updateList()
+    }
+
+    updateList = () => {
+        this.apiService
+            .getAllPersons()
+            .then((list) => {
+                this.setState({list})
+            })
+    }
+
     render() {
+        const {list} = this.state
         return (
             <div className="col-5">
                 <ul>
+                    {list.map((item) => {
+                        return (<li key={item.name}>{item.name}</li>)
+                    })}
                     <li>A</li>
                     <li>B</li>
                     <li>C</li>
