0889775a428d9c855005133659d2ab8792be0888
RinatSa <sadykov.r@seznam.cz>
2023-06-07 18:49:04 +0200

    Add logic (loading/error/component) Add updatePlanet function

--- src/components/random-planet.jsx
+++ src/components/random-planet.jsx
@@ -1,21 +1,54 @@
 import {Component} from 'react';
+import ApiService from "../apiService.js";
+import RandomPlanetComponent from "./random-planet-component.jsx";
+import Loader from "./loader";
+import ShowError from "./show-error.jsx";
 
 class RandomPlanet extends Component {
+
+    apiService = new ApiService()
+
+    state = {
+        planet: {
+            name: null,
+            population: null,
+            rotation: null,
+            diameter: null
+        },
+        loading: true,
+        error: false
+    }
+
+    componentDidMount() {
+        setInterval(this.updatePlanet, 5000)
+    }
+
+    updatePlanet = () => {
+        const id = Math.floor(3 + Math.random() * 17)
+        this.apiService
+            .getPlanet(id)
+            .then((planet) => {
+                this.setState({
+                    planet: {
+                        name: planet.name,
+                        population: planet.population,
+                        rotation: planet.rotation_period,
+                        diameter: planet.diameter,
+                        id: id
+                    },
+                    loading: false,
+                    error: false
+                })
+            })
+    }
+
     render() {
+        const {loading, error} = this.state
         return (
             <div className="row">
-                <div className="col d-flex align-items-center">
-                    <div>
-                        <img src="https://starwars-visualguide.com/assets/img/planets/2.jpg" alt=""
-                             className="planet"/>
-                    </div>
-                    <div className="info">
-                        <h1>Endor</h1>
-                        <p>Population: 216451314354</p>
-                        <p>Rotation Period: 15</p>
-                        <p>Diameter: 5646546 km</p>
-                    </div>
-                </div>
+                {loading ? <Loader/> : null}
+                {error ? <ShowError/> : null}
+                {!(loading || error) ? <RandomPlanetComponent planetData={this.state.planet}/> : null}
             </div>
         );
     }
