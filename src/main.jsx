1965eddd0209a24aa8d50629dc33a35b2cd1c623
RinatSa <sadykov.r@seznam.cz>
2023-06-08 16:36:12 +0200

    Update: add Routing and router. Set paths and elements.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import PeoplePage from "./components/pages/people-page.jsx";
+import StarshipPage from "./components/pages/starship-page.jsx";
+import PlanetPage from "./components/pages/planet-page.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [{
+            path: "/people",
+            element: <PeoplePage/>,
+        }, {
+            path: "/starships",
+            element: <StarshipPage/>
+        }, {
+            path: "/planets",
+            element: <PlanetPage/>
+        }],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
