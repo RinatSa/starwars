dadf02b3749386dd3d58f156b4f645db24f5e5a2
RinatSa <sadykov.r@seznam.cz>
2023-06-08 16:32:08 +0200

    Update: add Links

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,19 @@
 import {Component} from 'react';
+import {Link} from "react-router-dom";
 
 class Header extends Component {
     render() {
         return (
             <div className="row d-flex">
                 <div className="col-2">
-                    Star DB
+                    <Link to="/">Star DB</Link>
                 </div>
                 <div className="col-10">
                     <nav>
                         <ul className="d-flex">
-                            <li>People</li>
-                            <li>Planets</li>
-                            <li>Ships</li>
+                            <li><Link to="/people">People</Link></li>
+                            <li><Link to="/starships">Starship</Link></li>
+                            <li><Link to="/planets">Planets</Link></li>
                         </ul>
                     </nav>
                 </div>
